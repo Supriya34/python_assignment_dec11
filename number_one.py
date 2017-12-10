c99d6888987f69140cff1b2167f321f1a0ed2f09
supriya <supriyadhakal45@gmail.com>
2017-12-10 22:21:28 +0545

    question1

--- number_one.py
+++ number_one.py
@@ -1,11 +1,9 @@
-
-
 def even_odd(number):
-    if number%2==0 :
+    if number%2==0:
         print("the number is even")
 
     else:
         print("the nmber is odd")
 
-number=input("Enter a number")
-even_odd(4)
+number=int(input("Enter a number"))
+even_odd(number)
